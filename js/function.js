4e3f4c4259db39343c4b338c982fccc297ca2ed5
diego combe <combe202@gmail.com>
2023-03-21 16:09:28 -0300

    main

--- js/function.js
+++ js/function.js
@@ -1,6 +1,13 @@
 
 document.getElementById("boton").onclick = function () {
     window.print()  
-} 
+};
 
-  
+document.getElementById("boton_color").onclick = function () {
+    document.body.style.background = 'black'
+};
+
+document.getElementById("boton_color2").onclick = function () {
+    document.body.style.background = 'lightgray'
+};
+ 
